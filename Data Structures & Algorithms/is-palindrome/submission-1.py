70560da67bc23d7de603fa697ca2e0852b5ac2d7
shauntjw18 <shaun.teojiwu@gmail.com>
2026-07-17 23:05:32 +0800

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left,right=0,len(s)
+        while left<right:
+            if str[left]!=str[right]:
+                return False
+            else:
+                left+=1
+                right-=1    
+        return True
+
+        
