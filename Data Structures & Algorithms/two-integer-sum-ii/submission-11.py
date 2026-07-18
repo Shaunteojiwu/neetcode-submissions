aaa188f821519e22eb17b80e698037bcf0e35b61
shauntjw18 <shaun.teojiwu@gmail.com>
2026-07-18 21:45:18 +0800

    Add: two-integer-sum-ii - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-11.py
@@ -0,0 +1,41 @@
+# class Solution:
+#     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#         dic={}
+#         for idx,i in enumerate(numbers):
+#             if i not in dic:
+#                 dic[i]=idx
+#         for idx,i in enumerate(numbers):
+#             complement=target-i
+#             if complement in dic:
+#                 return [dic[i]+1,dic[complement]+1]
+
+# class Solution(object):
+#     def twoSum(self, numbers, target):
+#         """
+#         :type numbers: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         left,right=0,len(numbers)-1
+#         while left<right:
+#             if numbers[left]+numbers[right]==target:
+#                return [left+1,right+1]
+#             elif numbers[left]+numbers[right]<target:
+#                 left+=1
+#             else:
+#                 right-=1
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        map={}
+        for idx,i in enumerate(numbers):
+            complement=target-i
+            if complement in map:
+                return [map[complement],idx]
+            map[i]=idx
+        
+                
+
+
+
+        
